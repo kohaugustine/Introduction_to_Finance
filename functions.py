8c55751e61c9d8c684b935498c19606bab153e8f
Kaugust <kohaugustine@yahoo.com.sg>
2015-07-05 19:23:15 +0800

    Added in ability for future_value function to also compute annuities, and reimplemented some original loops as list comprehensions

--- functions.py
+++ functions.py
@@ -7,26 +7,41 @@ Date created is 22 Jun 2015.
 from decimal import Decimal, getcontext
 getcontext().prec = 20	# Set the precision to very high order for maximal accuracy
 
-def future_value(pv, r, n):
+def future_value(r, n, pv = None, pmt = None):
 	'''
 	Function to compute the Future Value based on interest rate and 
 	present value. 
 
 	Arguments accepted
 	------------------
-	* pv = present value in dollars
-
 	* r = interest rate, which should be given in its original percentage, eg. 
 	5% instead of 0.05
 
 	* n = number of periods for which the cash flow, either as annuity or single
 	flow from one present value
+
+	* pv = present value in dollars, if problem is annuity based, leave this empty
+
+	* pmt = each annuity payment in dollars, if problem is present value based, 
+	leave this empty
 	'''
+	original_args = [r, n, pv, pmt]
 	# Convert all integers or floats into Decimal objects to maximize precision and
-	# eliminate rounding off errors in between computations
-	pv, r, n = Decimal(pv), Decimal(r), Decimal(n)
-	# leave 100 as integer since Decimal objects cannot be divided by floats
-	return (1 + (r/100))**n * pv
+	# eliminate rounding off errors in between computations, placing the None values 
+	# unchanged as they are back in the same position of the list
+	dec_args = [Decimal(arg) if arg != None else arg for arg in original_args]
+
+	if dec_args[3] == None:
+		# leave 100 as integer since Decimal objects cannot be divided by floats
+		return (1 + (dec_args[0]/100))**dec_args[1] * dec_args[2]
+	elif dec_args[2] == None:
+		annuity_length = range(1, dec_args[1]+1)
+		# Apply compounding to each annuity payment made based on the number of 
+		# years left till end 
+		all_compounded_annuity = [(1 + (dec_args[0]/100))**(dec_args[1]-time_left) * dec_args[3] for time_left in annuity_length]
+		return sum(all_compounded_annuity)
+
+		
 
 def present_value(future_value, interest_rate, period_count):
 	'''
